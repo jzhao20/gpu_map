48bf53072d8861504d97b68ba090996db6f44791
ericlchen <ericlchen@utexas.edu>
2021-09-22 19:32:04 -0500

    Added working example project

---
+++ demo/src/test/java/com/gpu_map/demo/GpuMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpu_map.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpuMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
